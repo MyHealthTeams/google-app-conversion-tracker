7840cce5eb2871d0cbeb3a24ac2a129cb533a26b
mht-pair <pairing@myhealthteams.com>
2015-06-03 09:16:16 -0700

    reportWithConversionID - name changed

--- src/ios/GoogleAppTracking.m
+++ src/ios/GoogleAppTracking.m
@@ -16,7 +16,7 @@
     [self.commandDelegate runInBackground:^{
 
       // see: https://developers.google.com/app-conversion-tracking/
-      [ACTConversionReporter reportWithConversionId:conversion_id label:tracking_label value:tracking_value isRepeatable:repeatable];
+      [ACTConversionReporter reportWithConversionID:conversion_id label:tracking_label value:tracking_value isRepeatable:repeatable];
 
       CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
       [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
